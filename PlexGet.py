b54c7a3cecd622d0cb1357a42ad479b45732c671
elias <14819853+eliasthecactus@users.noreply.github.com>
2023-01-23 17:08:07 +0100

    file creation success

--- PlexGet.py
+++ PlexGet.py
@@ -1,16 +1,13 @@
 #!/usr/bin/env python
-# -*- coding: utf-
 
-#import libraries
-import argparse
 from common import *
-
+config = configparser.ConfigParser(strict=False)
 #get arguments
 parser = argparse.ArgumentParser()
 parser.add_argument("--mode", help="port for the operating mode (gui/cli)", type=str, choices=["gui", "cli"], default="gui")
 parser.add_argument("--port", help="port on which the webinterface should run", type=int, default=80)
-parser.add_argument("--config", type=pathlib.Path, default=pathlib.Path(__file__).absolute().parent, help="Path where the config should be stored")
-parser.add_argument("--library", type=pathlib.Path, default=pathlib.Path(__file__).absolute().parent, help="Path where the library infos should be stored")
+parser.add_argument("--config", type=pathlib.Path, default=pathlib.Path(__file__).absolute().parent / pathlib.Path("data") / pathlib.Path("config"), help="Path where the config should be stored")
+parser.add_argument("--library", type=pathlib.Path, default=pathlib.Path(__file__).absolute().parent / pathlib.Path("data") / pathlib.Path("library"), help="Path where the library infos should be stored")
 args = parser.parse_args()
 
 #function to check if arguments are correct
@@ -25,27 +22,132 @@ def check_args():
     if type(args.port) != int or not args.port < 99999:
         parser.error("--port not valid. choose port between 1 and 99999.")
     
-    #check if config path is valid
-    if not args.config.is_dir():
-        parser.error("--config not valid. choose a valid path.")
-
-    #check if library path is valid
-    if not args.library.is_dir():
-        parser.error("--library not valid. choose a valid path.")
+    #create path structure if it doesn't exist
+    args.config.mkdir(parents=True, exist_ok=True)
+    args.library.mkdir(parents=True, exist_ok=True)
 
 
 
-    
-    
+def configure():
+    if checkFileExists(args.config / pathlib.Path("config.ini")):
+        os.remove(args.config / pathlib.Path("config.ini"))
+        clear()
+        print("Deleted config. Please restart script")
+        exit()
+    """
+    try:
+        os.remove(args.config / pathlib.Path("config.ini"))
+    except:
+        pass
+    """
+    try:
+        os.remove(args.library / pathlib.Path("media.json"))
+    except:
+        pass
+    try:
+        os.remove(args.library / pathlib.Path("libraries.json"))
+    except:
+        pass
+    try:
+        os.remove(args.library / pathlib.Path("servers.json"))
+    except:
+        pass
+    donotexit = 1
 
+    while donotexit == 1:
+        try:
+            config.read(args.config / pathlib.Path("config.ini"))
+            try:
+                config.read(args.config / pathlib.Path("config.ini"))
+                config["CREDENTIALS"]["username"]
+                try:
+                    config.read(args.config / pathlib.Path("config.ini"))
+                    config["SERVER"]["url"]
+                    try:
+                        config["LIBRARY"]["id"]
+                        try:
+                            mediaList(config["SERVER"]["url"], config["LIBRARY"]["id"], config["SERVER"]["token"], args.library)
+                            donotexit = 0
+                        except:
+                            logging.error("Error with getting media")
+                    except:     
+                        getLibrarys(config["SERVER"]["url"], config["SERVER"]["token"], args.library)
+                        data = list(jsonToDict(args.library / pathlib.Path("libraries.json")))
+                        printMenu("","")
+                        for i, x in enumerate(data):
+                            print(str(i)+": "+str(x['title']))
+                        choice1 = int(input("Please choose a library: "))
+                        saveLibrary(str(data[int(choice1)]['title']), str(data[int(choice1)]['id']), args.config)
+                except:
+                    get_servers(getUser(args.config), getPass(args.config), args.library)
+                    data = list(jsonToDict(args.library / pathlib.Path("servers.json")))
+                    printMenu("","")
+                    for i, x in enumerate(data):
+                        print(str(i)+": "+str(x['name']))
+                    choice1 = int(input("Please choose a Server: "))
+                    server_name = data[choice1]['name']
+                    server_token = data[choice1]['token']
+                    printMenu("","")
+                    for i, x in enumerate(data[choice1]['connections']):
+                        print(str(i)+": "+str(x['protocol'])+"://"+str(x['address'])+":"+str(x['port']))
+                    choice2 = int(input("Please choose a connection: "))
+                    server_url = data[choice1]['connections'][choice2]['protocol']+"://"+data[choice1]['connections'][choice2]['address']+":"+data[choice1]['connections'][choice2]['port']
+                    saveServer(server_url, server_token, server_name, args.config)
+            except:
+                printMenu("","")
+                saveCred(str(input("Plex Username/Mail: ")),str(input("Plex Password: ")),args.config)
+                time.sleep(1)
+        except:
+            printMenu("","")
+            saveCred(str(input("Plex Username/Mail: ")),str(input("Plex Password: ")),args.config)
+    main()
+
+def printMenu(title, choices):
+    clear()
+    print("""
+██████╗ ██╗     ███████╗██╗  ██╗ ██████╗ ███████╗████████╗
+██╔══██╗██║     ██╔════╝╚██╗██╔╝██╔════╝ ██╔════╝╚══██╔══╝
+██████╔╝██║     █████╗   ╚███╔╝ ██║  ███╗█████╗     ██║   
+██╔═══╝ ██║     ██╔══╝   ██╔██╗ ██║   ██║██╔══╝     ██║   
+██║     ███████╗███████╗██╔╝ ██╗╚██████╔╝███████╗   ██║   
+╚═╝     ╚══════╝╚══════╝╚═╝  ╚═╝ ╚═════╝ ╚══════╝   ╚═╝   
+    by eliasthecactus
+    """)
+    print()
+    print("config: "+str(args.config))
+    print("library: "+str(args.library))
+    print("mode: "+str(args.mode))
+    print("\n==============="+title+"===============")
+    print(choices)
+    print("")
 
 
 def main():
     check_args()
+
     #run cli mode
     if args.mode == "cli":
-        #config(args.config)
-        pass
+        if getUser(args.config) and getPass(args.config):
+            if checkCred(getUser(args.config), getPass(args.config)):
+                if checkFileExists(args.library / pathlib.Path("media.json")):
+                    printMenu("Use existing Configuration?","" )
+                    choice3 = str(input("y/n: "))
+                    if choice3.lower() == "y":
+                        pass
+                    elif choice3.lower() == "n":
+                        configure()
+                    else:
+                        print("Wrong choice")
+                        exit()
+                else:
+                    configure()
+            else:
+                logging.error("Wrong Credentials. Please retry")
+                configure()
+        else:
+            configure()
+            
+
     #run gui mode
     elif args.mode == "gui":
         pass
--- common.py
+++ common.py
@@ -5,18 +5,38 @@ import string
 from xml.etree import ElementTree
 import logging
 import json
+import re
+import urllib.parse
+import codecs
+import base64
+import argparse
+import configparser
+import os
+import shutil
+import time
+import platform
+
+
+config = configparser.ConfigParser(strict=False)
 
 logging.basicConfig(filename="PlexGet.log", format='%(asctime)s %(levelname)-8s %(message)s', datefmt='%Y-%m-%d %H:%M:%S', encoding="utf-8", level=logging.INFO)
+logging.getLogger().addHandler(logging.StreamHandler())
 
 
+def clear():
+    if platform.system() == "Windows":
+        os.system('cls')
+    else:
+        os.system('clear')
 
 def random_generator(size=20, chars=string.ascii_uppercase + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
 
+
     
 #function to get plex-token of your account
-def get_servers(user, password):
-    try:
+def get_servers(user, password, path):
+    #try:
         url = "https://plex.tv/users/sign_in.json"
         payload = "user%5Bpassword%5D="+password+"&user%5Blogin%5D="+user
         headers = {
@@ -63,15 +83,15 @@ def get_servers(user, password):
         #add the resource to the json
         servers.append(temp_dict)
 
-        with open("servers.json", "w") as servers_file:
+        with open(path / "servers.json", "w") as servers_file:
             json.dump(servers, servers_file)
-        return True
-    except:
-        logging.error("Error with getting server")
-        return False
+        return servers
+    #except:
+    #    logging.error("Error with getting server. Please ceck your credentials and try again")
+    #    return False
         
 
-def getLibrarys(server, token):
+def getLibrarys(server, token, path):
     try:
         url = server+"/library/sections"
         headers = {
@@ -90,18 +110,19 @@ def getLibrarys(server, token):
             temp_dict["title"] = y.attrib["title"]
             temp_dict["id"] = y.attrib["key"]
             temp_dict["language"] = y.attrib["language"]
+            temp_dict["type"] = y.attrib["type"]
             libraries.append(temp_dict)
 
-        with open("libraries.json", "w") as libraries_file:
+        with open(path / "libraries.json", "w") as libraries_file:
             json.dump(libraries, libraries_file)
         return True
 
     except:
-        logging.error("cannot get librarys")
+        logging.error("Error with getting libraries. Make sure you have some libraries which you can access")
         return False
 
-def mediaList(server, id, token):
-    #try:
+def mediaList(server, id, token, path):
+    try:
         url = server+"/library/sections/"+ id +"/all"
         headers = {
             'X-Plex-Version': '4.99.2',
@@ -115,43 +136,160 @@ def mediaList(server, id, token):
         
         media = []
         for x in xml_response.iter("Video"):
-            key = x[0][0].attrib["key"]
-            file = x[0][0].attrib["file"]
-            print(key)
+
+            
+            key = re.match("(.*\/)(.*?)$", x[0][0].attrib["key"]).group(1)
+            file = re.match("(.*\/)(.*?)$", x[0][0].attrib["file"]).group(2)
+            movie_identifier = key+file
+            downloadurl = server+urllib.parse.quote(movie_identifier)+"?download=0&X-Plex-Token="+token
 
 
             temp_dict = {}
-            temp_dict["title"] = x.attrib["title"]
             try:
-                temp_dict["art_url"] = x.attrib["art"]
+                temp_dict["title"] = x.attrib['title']
+            except:
+                temp_dict["title"] = ""
+            try:
+                temp_dict["year"] = x.attrib['year']
+            except:
+                temp_dict["year"] = ""
+            try:
+                temp_dict["rating"] = x.attrib['audienceRating']
+            except:
+                temp_dict["rating"] = ""
+            try:
+                temp_dict["summary"] = x.attrib['summary']
+            except:
+                temp_dict["summary"] = ""
+            try:
+                temp_dict["download_url"] = downloadurl
+            except:
+                temp_dict["download_url"] = ""
+            try:
+                temp_dict["art_url"] = server+x.attrib["art"]+"?X-Plex-Token="+token
             except:
                 temp_dict["art_url"] = ""
             try:
-                temp_dict["thumb_url"] = x.attrib["thumb"]
+                temp_dict["thumb_url"] = server+x.attrib["thumb"]+"?X-Plex-Token="+token
             except:
                 temp_dict["thumb_url"] = ""
+            try:
+                temp_dict["duration"] = round((int(x.attrib["duration"])/1000)/60)
+            except:
+                temp_dict["duration"] = ""
+            try:
+                temp_list2 = []
+                for z in x.iter("Genre"):
+                    temp_list2.append(z.attrib["tag"])
+                temp_dict["genres"] = temp_list2
+            except:
+                temp_dict["genres"] = ""
+
             media.append(temp_dict)
 
-        #with open("media.json", "w") as media_file:
-        #    json.dump(media, media_file)
+        with codecs.open(path / "media.json", "w", encoding="UTF-8") as media_file:
+            json.dump(media, media_file, ensure_ascii=False)
         return True
 
-    #except:
-    #    logging.error("cannot get librarys")
-    #    return False
+    except:
+        logging.error("Error with getting media. Please make sure that your selected library contains some media files")
+        return False
     
-    
+
+def encode64(string):
+    return base64.b64encode(string.encode('utf-8')).decode("utf-8")
+
+def decode64(string):
+    return base64.b64decode(string).decode('utf-8')
 
 
+def getUser(path):
+    try:
+        config.read(pathlib.Path(path / pathlib.Path("config.ini")))
+        return decode64(config['CREDENTIALS']['username'])
+    except:
+        False
 
+def getPass(path):
+    try:
+        config.read(pathlib.Path(path / pathlib.Path("config.ini")))
+        return decode64(config['CREDENTIALS']['password'])
+    except:
+        False
 
+def getToken(path):
+    try:
+        config.read(pathlib.Path(path / pathlib.Path("config.ini")))
+        return decode64(config['SERVER']['token'])
+    except:
+        return False
 
-#function to create the config file
-def config(config_path):
-    #config file path
-    config_file = config_path / pathlib.Path("config")
-    #create config file if it doesn't exist
-    if not config_file.is_file():
-        with open(config_file, 'w') as fp:
-            fp.write('')
+def getURL(path):
+    try:
+        config.read(pathlib.Path(path / pathlib.Path("config.ini")))
+        return decode64(config['SERVER']['url'])
+    except:
+        return False
 
+def saveServer(url, token, name, path):
+    #config['SERVER'] = {'URL': url, 'token': token}
+    config.read(pathlib.Path(path / pathlib.Path("config.ini")))
+    try:
+        config.add_section('SERVER')
+    except:
+        pass
+    config.set('SERVER', 'url' , url)
+    config.set('SERVER', 'name' , name)
+    config.set('SERVER', 'token' , token)
+    with open(pathlib.Path(path / pathlib.Path("config.ini")), 'w') as f:
+        config.write(f)
+
+def saveCred(user, password, path):
+    #config['CREDENTIALS'] = {'username': encode64(user), 'password': encode64(password)}
+    config.read(pathlib.Path(path / pathlib.Path("config.ini")))
+    try:
+        config.add_section('CREDENTIALS')
+    except:
+        pass
+    config.set('CREDENTIALS', 'username' ,encode64(user))
+    config.set('CREDENTIALS', 'password' ,encode64(password))
+    with open(pathlib.Path(path / pathlib.Path("config.ini")), 'w') as f:
+        config.write(f)
+
+def saveLibrary(name, id, path):
+    config.read(pathlib.Path(path / pathlib.Path("config.ini")))
+    try:
+        config.add_section('LIBRARY')
+    except:
+        pass
+    config.set('LIBRARY', 'name' , name)
+    config.set('LIBRARY', 'id' , id)
+    with open(pathlib.Path(path / pathlib.Path("config.ini")), 'w') as f:
+        config.write(f)
+
+#check if creds are valid
+def checkCred(user, password):
+    url = "https://plex.tv/users/sign_in.json"
+    payload = "user%5Bpassword%5D="+password+"&user%5Blogin%5D="+user
+    headers = {
+        'X-Plex-Version': '4.99.2',
+        'X-Plex-Product': 'Plex Web',
+        'X-Plex-Client-Identifier': random_generator(26),
+        'Content-Type': 'application/x-www-form-urlencoded',
+    }
+    response = requests.request("POST", url, headers=headers, data=payload)
+    if str(response.status_code).startswith('2'):
+        return True
+    else:
+        #logging.error((response.content).decode('utf-8'))
+        return False
+
+def jsonToDict(file):
+    with open(file) as json_file:
+        return json.load(json_file)
+
+def checkFileExists(file):
+    if file.is_file():
+        return True
+    else:
+        return False
